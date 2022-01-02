a0c523c94715d8318255ba0d48fa4ae0105b8ffe
douglasddrdias <douglasddrdias@gmail.com>
2022-01-02 20:35:39 -0300

    Materiais do curso de instroduçao a sintaxe javascript

---
+++ curso-sintaxe-basica-javascript/aula 2/script.js
@@ -0,0 +1,17 @@
+// tipos primitivos
+
+//boolean
+/*var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+var nome = 'diana'
+console.log(typeof(nome));*/
+
+// como declara
+/*var variavel = 'diana'
+variavel = 'martine'
+console.log(variavel);*/
