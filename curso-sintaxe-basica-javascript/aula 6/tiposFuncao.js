aa89bd1ec8432a2106b166b53bbeb1453cc37389
douglasddrdias <douglasddrdias@gmail.com>
2022-01-03 17:03:32 -0300

    Materiais do curso de instroduçao a sintaxe javascript

---
+++ curso-sintaxe-basica-javascript/aula 6/tiposFuncao.js
@@ -0,0 +1,21 @@
+// tipos de função
+
+// declarativas
+// function funcao(){
+//     console.log('Sou uma mensagem de uma função declarativa!');
+// }
+
+// funcao();
+
+// expressão de função
+// var funcao = function (){
+//     console.log('Sou uma mensagem de função de expressão!');
+// }
+// funcao();
+
+// arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function');
+}
+
+funcao();
