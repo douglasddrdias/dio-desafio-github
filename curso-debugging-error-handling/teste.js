c4222e1ce0dc7248e9fbe244aed7437b8f434b52
douglasddrdias <douglasddrdias@gmail.com>
2022-01-07 12:41:48 -0300

    Materiais do curso de funcoes

--- curso-debugging-error-handling/teste.js
+++ curso-debugging-error-handling/teste.js
@@ -1,5 +1,15 @@
-const NovoErro = new Error();
-
-NovoErro.name = "Novo Erro";
-NovoErro.message = "Este erro nunca ocorreu"
-throw NovoErro;
+function validaArray(arr, tamanho) {
+    try {
+      if (!arr.length || arr.length !== tamanho) throw new RangeError("O tamanho do array é inválido");
+      console.log('vazio');
+      return arr;
+    } catch(e) {
+      if (e instanceof RangeError) {
+        return console.log(e.message);
+      } else {
+        return e;
+      }
+    }
+  } 
+  
+  try {validaArray();} catch(e) { console.log();}
