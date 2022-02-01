82b2101f02120c24c583f5f8c71b4c818e101bd8
douglasddrdias <douglasddrdias@gmail.com>
2022-01-31 22:36:59 -0300

    Criaçao de todos os endpoints de usuarios

--- curso-arquitetura-rest-node/src/index.ts
+++ curso-arquitetura-rest-node/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express'
+import usersRoute from './routes/users.route';
 
 const app = express();
+// adicionando um midleware para interpretar json
+app.use(express.json());
+// configurando para o express entender query string
+app.use(express.urlencoded({ extended: true }));
+//configuração de rotas
+app.use(usersRoute);
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Opa deu certo' });
+    res.status(200).send({ foo: 'Sucesso' });
 })
 
+//inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 });
